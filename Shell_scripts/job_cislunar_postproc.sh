22468eb18af8643709c3a689991678dcbdb7523a
LorenzoFederici <lorenzo.federici@uniroma1.it>
2022-03-09 18:03:27 +0100

    ok

--- Shell_scripts/job_cislunar_postproc.sh
+++ Shell_scripts/job_cislunar_postproc.sh
@@ -5,11 +5,11 @@ i_min=0
 i_max=0
 
 
-dirTR[0]="results/PPO_2022-02-17_18-57-41/"  
+dirTR[0]="results/PPOTrainer_2022-03-08_16-41-43/"  
 
-dirEXP[0]="results/PPO_2022-02-17_18-57-41/PPO_2022-02-18_11-49-21/PPO_environment.cislunar_env.CislunarEnv_6e249_00000_0_2022-02-18_11-49-21/"
+dirEXP[0]="results/PPOTrainer_2022-03-08_16-41-43/PPOTrainer_environment.cislunar_env.CislunarEnv_41033_00000_0_2022-03-08_16-41-43/"
 
-CP[0]=2000
+CP[0]=497
 
 for ((i = i_min ; i <= i_max ; i+=1)); do
     python main_postproc.py --trainer_dir ${dirTR[$i]} --exp_dir ${dirEXP[$i]} --last_cp ${CP[$i]}
