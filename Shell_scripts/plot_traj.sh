41034609057f925b32e5b35103b32c5b8f1c94d4
clp98 <carlo@ENGR-FURFARO05G.BLUECAT.arizona.edu>
2023-06-15 14:07:17 -0700

    update config per prova

--- Shell_scripts/plot_traj.sh
+++ Shell_scripts/plot_traj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# indir="results/PPO_2023-05-01_12-25-48/PPO_2023-05-01_13-41-48/PPO_environment.Moon_Station_Keeping_Env.Moon_Station_Keeping_Env_985ed_00000_0_2023-05-01_13-41-49/checkpoint_000353/"
-indir="results/PPO_2023-06-14_14-14-38/PPO_2023-06-15_02-40-09/PPO_environment.Moon_Station_Keeping_Env.Moon_Station_Keeping_Env_9e570_00000_0_2023-06-15_02-40-09/checkpoint_005928/"
+#indir="results/PPO_2023-05-01_12-25-48/PPO_2023-05-01_13-41-48/PPO_environment.Moon_Station_Keeping_Env.Moon_Station_Keeping_Env_985ed_00000_0_2023-05-01_13-41-49/checkpoint_000353/"
+indir="results/PPO_2023-06-14_16-38-30/PPO_2023-06-15_09-48-23/PPO_environment.Moon_Station_Keeping_Env.Moon_Station_Keeping_Env_714e5_00000_0_2023-06-15_09-48-23/checkpoint_001799/"
 gnuplot -e "indir=\"${indir}\"" "PlotFiles/plot_traj.plt"
 gnuplot -e "indir=\"${indir}\"" "PlotFiles/plot_traj_2d.plt"
 
