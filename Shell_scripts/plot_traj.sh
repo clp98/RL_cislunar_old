e4c14024782cf70cd954d891bf69891c592b2ec3
Carlo La Pegna <carlolapegna98@gmail.com>
2023-07-06 11:17:23 -0700

    prove definitive

--- Shell_scripts/plot_traj.sh
+++ Shell_scripts/plot_traj.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #indir="results/PPO_2023-05-01_12-25-48/PPO_2023-05-01_13-41-48/PPO_environment.Moon_Station_Keeping_Env.Moon_Station_Keeping_Env_985ed_00000_0_2023-05-01_13-41-49/checkpoint_000353/"
-indir="results/PPO_2023-06-28_14-29-00/PPO_2023-06-29_11-53-10/PPO_2023-06-29_12-41-48/PPO_environment.Moon_Station_Keeping_Env.Moon_Station_Keeping_Env_fccb1_00000_0_2023-06-29_12-41-48/checkpoint_004969/"
+indir="results/PPO_2023-07-03_11-46-17/PPO_2023-07-03_15-26-10/PPO_2023-07-04_20-57-29/PPO_environment.Moon_Station_Keeping_Env.Moon_Station_Keeping_Env_0fa08_00000_0_2023-07-04_20-57-29/checkpoint_004976/"
 gnuplot -e "indir=\"${indir}\"" "PlotFiles/plot_traj.plt"
 gnuplot -e "indir=\"${indir}\"" "PlotFiles/plot_traj_2d.plt"
 
--- Shell_scripts/run_sims.sh
+++ Shell_scripts/run_sims.sh
@@ -6,5 +6,14 @@
 #Halo, 4 body, errore, LSTM, max_r=500km, lr decrescente(-4,-5), epsilon decrescente(0.1,0.001)  [2023-06-16_17-13-17]
 #Halo, 4 body, errore, LSTM, max_r=500km, lr decrescente(-4,-5), epsilon decrescente(0.1,0.001), wild_reward [PPO_2023-06-21_15-48-54]
 
-python main_solve_RL_auto.py --config "config_files/moon_station_keeping_config_COMPUTERONE1.yaml"  #4 body, error i.c., max_dist_r, clip=0.05, lr=0.0001 cost, epsilon=(0.1,0.001), MLP
-python main_solve_RL_auto.py --config "config_files/moon_station_keeping_config_COMPUTERONE2.yaml"  #4 body, error i.c., max_dist_r, clip=0.05, lr=0.0001 cost, epsilon=(0.1,0.001), LSTM
+#4 body, error i.c., max_dist_r, clip=0.05, lr=0.0001 cost, epsilon=(0.1,0.001), MLP [PPO_2023-06-29_13-48-39]
+#4 body, error i.c., max_dist_r, clip=0.05, lr=0.0001 cost, epsilon=(0.1,0.001), LSTM [PPO_2023-06-29_13-48-39]
+
+#max_seq_len: 10 [LSTM_len10_size20]
+#lstm_cell_size: 10 [LSTM_len25_size10]
+
+#lmax_seq_len: 35 [LSTM_len35_size20]
+#PPO_2023-07-01_21-14-28 con max_seq_len=35, cell_size=10 [LSTM_len35_size10]
+
+python main_solve_RL_auto.py --config "config_files/moon_station_keeping_config_venus1.yaml"  
+python main_solve_RL_auto.py --config "config_files/moon_station_keeping_config_venus2.yaml"  
